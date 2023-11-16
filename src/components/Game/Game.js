9b3d8d077aa8b3267cfeba4b66d28ece0dae4d20
AlexRS90 <aramosbengy@gmail.com>
2023-11-16 16:17:35 -0600

    Display a win-lose banner

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,9 @@ import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import Form from '../Form/Form';
 import WordsList from '../WordsList/WordsList';
+import {NUM_OF_GUESSES_ALLOWED} from '../../constants';
+import WonBanner from '../WonBanner/WonBanner';
+import LoseBanner from '../LoseBanner/LoseBanner';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -12,14 +15,27 @@ console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
+  const [status, setStatus] = React.useState('running');
   function handleSubmitGuess(tentativeGuess){
     //todo
-    setGuesses([...guesses, tentativeGuess]);
+    const nextGuess = [...guesses, tentativeGuess];
+    setGuesses(nextGuess);
+    if(tentativeGuess === answer){
+      setStatus('won')
+    }else if(nextGuess.length >= NUM_OF_GUESSES_ALLOWED){
+      setStatus('lost');
+    }
   }
   return(
     <>
       <WordsList guesses={guesses} answer={answer} />
-      <Form handleSubmitGuess={handleSubmitGuess} />
+      <Form handleSubmitGuess={handleSubmitGuess} status={status} />
+      {status === 'won' &&
+        <WonBanner numOfGuesses={guesses.length} />
+      }
+      {status === 'lost' &&
+        <LoseBanner answer={answer} />
+      }
     </>
   );
 }
